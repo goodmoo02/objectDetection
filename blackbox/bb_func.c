49880c0e36ee1c6ebe78bc0bbbe86f7636ae7a89
goodmoo02 <goodmoo02@gmail.com>
2019-07-09 20:38:07 +0900

    blackbox

--- blackbox/bb_func.c
+++ blackbox/bb_func.c
@@ -23,7 +23,8 @@
 #define size 256
 #define event_dir "./event"
 #define always_dir "./always"
-
+#define max_len 256	
+#define pt "/home/jiwoong/objectDetection/blackbox/del_dir/"
 // ???? ????? ?? ???? ?? ?? ???.
 int indent = 0;
 int dir_size = 0;
@@ -260,14 +261,56 @@ int main(int argc, char* argv[])
 // struct dirent **namelist;
 // int count;
 // int idx;
-	
-int rm_olddir(char* dir, struct dirent ***namelist){
-    struct dirent **namelists;
-    int count;
-    int idx;
-    if((count = scandir(dir, &namelist, NULL, alphasort))==-1)
-    {
 
-        return 1;
+int rm_olddir(char* dir){
+    struct dirent **dir_list;
+    struct dirent **f_list;
+    // DIR *dir_info;
+    int dir_count;
+    int f_count;
+    int idx;
+    char dir_name[max_len] = { 0, };
+    char f_name[max_len] = {0, };
+    if((dir_count = scandir(dir, &dir_list, NULL, alphasort))==-1)
+    {
+        fprintf(stderr, "error: %s\n", strerror(errno));
+        return -1;
     }
+    printf("%s\n", "dir_scan_compelete");
+    
+    strcat(dir_name, pt);
+    strcat(dir_name, dir_list[2]->d_name);
+
+    if(rmdir(dir_name) == -1)
+        {
+            printf("dir has files..");
+            if((f_count = scandir(dir_name, &f_list, NULL, alphasort))==-1)
+            {
+                fprintf(stderr, "error: %s\n", strerror(errno));
+                return -1;
+            }
+            printf("%s\n", "file_scan_compelete");
+
+            for (idx=2; idx < f_count; idx++){
+                strcpy(f_name, dir_name);
+                strcat(f_name, "/");
+                strcat(f_name, f_list[idx]->d_name);
+                printf("%s\n", f_name);
+                if(remove(f_name) == -1)
+                {
+                    fprintf(stderr, "f_del_error: %s\n", strerror(errno));
+                    return -1;
+                }
+            }
+
+            printf("%s\n", "file_del_compelete");
+            if(remove(dir_name) == -1)
+                {
+                    fprintf(stderr, "error: %s\n", strerror(errno));
+                    return -1;
+                }
+            printf("%s\n", "dir_del_compelete");
+            return 0;
+        }
 }
+
--- blackbox/func.c
+++ blackbox/func.c
@@ -22,6 +22,8 @@
 #define size 256
 #define event_dir "./event"
 #define always_dir "./always"
+#define max_len 256	
+#define pt "/home/jiwoong/objectDetection/blackbox/del_dir/"
 
 // ???? ????? ?? ???? ?? ?? ???.
 int indent = 0;
@@ -141,3 +143,55 @@ struct f_size* avail(char* dirname)
 	return lf_size;
 }
 
+
+int rm_olddir(char* dir){
+    struct dirent **dir_list;
+    struct dirent **f_list;
+    // DIR *dir_info;
+    int dir_count;
+    int f_count;
+    int idx;
+    char dir_name[max_len] = { 0, };
+    char f_name[max_len] = {0, };
+    if((dir_count = scandir(dir, &dir_list, NULL, alphasort))==-1)
+    {
+        fprintf(stderr, "error: %s\n", strerror(errno));
+        return -1;
+    }
+    printf("%s\n", "dir_scan_compelete");
+    
+    strcat(dir_name, pt);
+    strcat(dir_name, dir_list[2]->d_name);
+
+    if(rmdir(dir_name) == -1)
+        {
+            printf("dir has files..");
+            if((f_count = scandir(dir_name, &f_list, NULL, alphasort))==-1)
+            {
+                fprintf(stderr, "error: %s\n", strerror(errno));
+                return -1;
+            }
+            printf("%s\n", "file_scan_compelete");
+
+            for (idx=2; idx < f_count; idx++){
+                strcpy(f_name, dir_name);
+                strcat(f_name, "/");
+                strcat(f_name, f_list[idx]->d_name);
+                printf("%s\n", f_name);
+                if(remove(f_name) == -1)
+                {
+                    fprintf(stderr, "f_del_error: %s\n", strerror(errno));
+                    return -1;
+                }
+            }
+
+            printf("%s\n", "file_del_compelete");
+            if(remove(dir_name) == -1)
+                {
+                    fprintf(stderr, "error: %s\n", strerror(errno));
+                    return -1;
+                }
+            printf("%s\n", "dir_del_compelete");
+            return 0;
+        }
+}
